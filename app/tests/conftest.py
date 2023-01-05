f7f10e89a7a87a92248bb06c85d87c08c34d7e60
Maksim Grafskiy <m.grafskiy@mts.ru>
2023-01-06 02:42:34 +0300

    1.5 add Movie model

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -5,16 +5,14 @@ from django.conf import settings
 import pytest
 
 
-DEFAULT_ENGINE = "django.db.backends.postgresql_psycopg2"
-
-
 @pytest.fixture(scope="session")
 def django_db_setup():
     settings.DATABASES["default"] = {
-        "ENGINE": os.environ.get("DB_TEST_ENGINE", DEFAULT_ENGINE),
-        "HOST": os.environ["DB_TEST_HOST"],
-        "NAME": os.environ["DB_TEST_NAME"],
-        "PORT": os.environ["DB_TEST_PORT"],
-        "USER": os.environ["DB_TEST_USER"],
-        "PASSWORD": os.environ["DB_TEST_PASSWORD"],
+        "ENGINE": os.environ.get("DB_TEST_ENGINE", settings.DATABASES["default"]["ENGINE"]),
+        "HOST": os.environ.get("DB_TEST_HOST", settings.DATABASES["default"]["HOST"]),
+        "NAME": os.environ.get("DB_TEST_NAME", settings.DATABASES["default"]["NAME"]),
+        "PORT": os.environ.get("DB_TEST_PORT", settings.DATABASES["default"]["PORT"]),
+        "USER": os.environ.get("DB_TEST_USER", settings.DATABASES["default"]["USER"]),
+        "PASSWORD": os.environ.get("DB_TEST_PASSWORD", settings.DATABASES["default"]["PASSWORD"]),
+        "ATOMIC_REQUESTS": settings.DATABASES["default"]["ATOMIC_REQUESTS"],
     }
